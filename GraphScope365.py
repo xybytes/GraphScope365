e79329461786d68cf57de4d753b89a93d920b27e
christianbortone <bortonechristian88@gmail.com>
2023-02-10 18:30:45 +0100

    create get_onedrive
    function to get Onedrive files + progress bar.

--- GraphScope365.py
+++ GraphScope365.py
@@ -1,7 +1,8 @@
 import requests
 import argparse
-import json
 import pandas as pd
+from tqdm import tqdm
+
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("-jwt", "--accessToken", help="Microsoft Graph access token")
@@ -33,18 +34,18 @@ def http_api(url, keyword=None):
 def get_site_id():
     response = http_api("https://graph.microsoft.com/v1.0/sites", config["filter"])
     list_site_id = []
-    while True:
-        data = response.json()
-        for item in data["value"]:
-            list_site_id.append(item["id"])
-            #print(item["webUrl"])
-            print(item["id"])
-        if '@odata.nextLink' in data:
-            url = data['@odata.nextLink']
-            response = requests.get(url, headers=headers)
-        else:
-            break
-    return list_site_id
+    with tqdm(total=None, desc="Getting Sharepoint IDs") as pbar:
+        while True:
+            data = response.json()
+            for item in data["value"]:
+                list_site_id.append(item["id"])
+            if '@odata.nextLink' in data:
+                url = data['@odata.nextLink']
+                response = requests.get(url, headers=headers)
+                pbar.update(1)
+            else:
+                break
+        return list_site_id
 
 def get_site_list(list_site_id):
     list_site_data = []
@@ -62,74 +63,110 @@ def get_file(site_data):
     for n in site_data:
         response = http_api("https://graph.microsoft.com/v1.0/sites/"+n["site_id"]+"/lists/"+n["list_id"]+"/items?$expand=driveItem")
         list_files = []
+        with tqdm(total=None, desc="Getting Sharepoint Files") as pbar:
+            while True:
+                data = response.json()
+                for file in data["value"]:
+                    file_data = []
+                    if file["contentType"]["name"] == "Document":
+                        file_data.append(file["fields"]["LinkFilename"])
+                        file_data.append(file["fields"]["FileSizeDisplay"])
+                        file_data.append(file["fields"]["DocIcon"])
+                        file_data.append(file["driveItem"]["shared"]["scope"])
+                        file_data.append(file["webUrl"])
+                        file_data.append(file["createdDateTime"])
+                        file_data.append(file["lastModifiedDateTime"])
+                        file_data.append(file["createdBy"]["user"]["email"])
+                        file_data.append(file["lastModifiedBy"]["user"]["email"])
+                        list_files.append(file_data)
+                    else:
+                        pass
+                if '@odata.nextLink' in data:
+                    url = data['@odata.nextLink']
+                    response = requests.get(url, headers=headers)
+                    pbar.update(1)
+                else:
+                    break
+        return list_files
+
+def get_emails():
+    response = http_api("https://graph.microsoft.com/v1.0/me/messages", config["filter"])
+    list_emails = []
+    with tqdm(total=None, desc="Getting Outlook Emails") as pbar:
+        while True:
+            data = response.json()
+            for email in data["value"]:
+                email_data = []
+                email_data.append(email["createdDateTime"])
+                email_data.append(email["from"]["emailAddress"]["address"])
+                toRecipients = ""
+                for recepient in email["toRecipients"]:
+                    toRecipients += recepient["emailAddress"]["address"]
+                    toRecipients += ";"
+                email_data.append(toRecipients)
+                ccRecipients = ""
+                for recepient in email["ccRecipients"]:
+                    ccRecipients += recepient["emailAddress"]["address"]
+                    ccRecipients += ";"
+                email_data.append(ccRecipients)
+                email_data.append(email["subject"])
+                email_data.append(email["bodyPreview"])
+                email_data.append(email["webLink"])
+                email_data.append(email["hasAttachments"])
+                list_emails.append(email_data)
+            if '@odata.nextLink' in data:
+                url = data['@odata.nextLink']
+                response = requests.get(url, headers=headers)
+                pbar.update(1)
+            else:
+                break
+        return list_emails
+
+
+def get_onedrive():
+    response = http_api("https://graph.microsoft.com/v1.0/me/drive/root/search(q='')")
+    list_files = []
+    with tqdm(total=None, desc="Getting OneDrive Files") as pbar:
         while True:
             data = response.json()
             for file in data["value"]:
                 file_data = []
-                if file["contentType"]["name"] == "Document":
-                    file_data.append(file["fields"]["LinkFilename"])
-                    file_data.append(file["fields"]["FileSizeDisplay"])
-                    file_data.append(file["fields"]["DocIcon"])
-                    file_data.append(file["driveItem"]["shared"]["scope"])
-                    file_data.append(file["webUrl"])
+                if "file" in file:
+                    file_data.append(file["name"])
+                    file_data.append(file["size"])
+                    file_data.append(file["file"]["mimeType"])
                     file_data.append(file["createdDateTime"])
                     file_data.append(file["lastModifiedDateTime"])
                     file_data.append(file["createdBy"]["user"]["email"])
                     file_data.append(file["lastModifiedBy"]["user"]["email"])
-                    print(file["webUrl"])
+                    file_data.append(file["webUrl"])
                     list_files.append(file_data)
                 else:
                     pass
             if '@odata.nextLink' in data:
                 url = data['@odata.nextLink']
                 response = requests.get(url, headers=headers)
+                pbar.update(1)
             else:
                 break
     return list_files
-
-def get_emails():
-    response = http_api("https://graph.microsoft.com/v1.0/me/messages", config["filter"])
-    list_emails = []
-    while True:
-        data = response.json()
-        for email in data["value"]:
-            email_data = []
-            email_data.append(email["createdDateTime"])
-            email_data.append(email["from"]["emailAddress"]["address"])
-            toRecipients = ""
-            for recepient in email["toRecipients"]:
-                toRecipients += recepient["emailAddress"]["address"]
-                toRecipients += ";"
-            email_data.append(toRecipients)
-            ccRecipients = ""
-            for recepient in email["ccRecipients"]:
-                ccRecipients += recepient["emailAddress"]["address"]
-                ccRecipients += ";"
-            email_data.append(ccRecipients)
-            email_data.append(email["subject"])
-            email_data.append(email["bodyPreview"])
-            email_data.append(email["webLink"])
-            email_data.append(email["hasAttachments"])
-            print(email_data)
-            list_emails.append(email_data)
-        if '@odata.nextLink' in data:
-            url = data['@odata.nextLink']
-            response = requests.get(url, headers=headers)
-        else:
-            break
-    return list_emails
 
 def export_data(data_array,fields,file_name):
     df = pd.DataFrame(data_array, columns = fields)
     df.to_excel(excel_writer = file_name, index=False)
 
-#list_site_id = get_site_id()
-#array_site = get_site_list(list_site_id)
-#output_files = get_file(array_site)
-output_emails = get_emails()
+list_site_id = get_site_id()
+array_site = get_site_list(list_site_id)
+output_files = get_file(array_site)
+columns_sharepoint = ["File Name","Size", "File type", "Shared", "URL", "Created Date Time", "Last Modified Date Time", "Created By", "Last Modified By"] 
+export_data(output_files,columns_sharepoint,"sharepoint.xlsx")
 
-columns_outlook = ["Created Date Time" , "From", "To" , "CC", "Subject", "Body Preview", "URL", "Attachments"]
-#columns_sharepoint = ["File Name","Size", "File type", "Shared", "URL", "Created Date Time", "Last Modified Date Time", "Created By", "Last Modified By"] 
 
-#export_data(output_files,columns_sharepoint,"sharepoint.xlsx")
-export_data(output_emails,columns_outlook,"outolok_emails.xlsx")
+#output_emails = get_emails()
+#columns_outlook = ["Created Date Time" , "From", "To" , "CC", "Subject", "Body Preview", "URL", "Attachments"]
+#export_data(output_emails,columns_outlook,"outolok_emails.xlsx")
+
+#output_onedrive = get_onedrive()
+#columns_onedrive = ["File Name","Size", "File type", "Created Date Time", "Last Modified Date Time", "Created By", "Last Modified By", "URL"] 
+#export_data(output_onedrive,columns_onedrive,"onedrive.xlsx")
+
